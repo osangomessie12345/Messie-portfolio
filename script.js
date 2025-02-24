2bf78d614f2ebbb06517b8303568272e974408bf
Osangomessie <messieosango659@gmail.com>
2025-02-25 00:05:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-    document.getElementById('contactForm').addEventListener('submit', function(event) {
-      event.preventDefault();
-      
-      // Collect form data
-      const name = document.getElementById('name').value;
-      const email = document.getElementById('email').value;
-      const message = document.getElementById('message').value;
-      
-      // WhatsApp API URL
-      const phoneNumber = '+243897520168'; // Replace with your WhatsApp number
-           const whatsappURL = `https://wa.me/${phoneNumber}?text=%0AMessage:%20${encodeURIComponent(message)}`;
-      
-      // Redirect user to WhatsApp chat
-      window.open(whatsappURL, '_blank');
-    });
+AOS.init({ duration: 1000 });
+
+function closeAlert() {    
+    document.getElementById("overlay").style.display = "none"; // Cache l'alerte
+    document.getElementById("content").style.display = "block"; // Affiche le contenu
+    document.body.style.overflow = "auto"; // Réactive le scroll
+    AOS.refresh(); // Recharge AOS pour bien appliquer les animations
+}
