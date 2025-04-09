1589e912e26219cd68ea0ba4a6ecc3edfa188b38
osangomessie12345 <servicesocial099@gmail.com>
2025-04-09 21:30:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-AOS.init({ duration: 1000 });
+AOS.init({
+            duration: 1000,
+            once: false,
+            offset: 120,
+            mirror: false
+        });
 
-function closeAlert() {    
-    document.getElementById("overlay").style.display = "none"; // Cache l'alerte
-    document.getElementById("content").style.display = "block"; // Affiche le contenu
-    document.body.style.overflow = "auto"; // Réactive le scroll
-    AOS.refresh(); // Recharge AOS pour bien appliquer les animations
-}
+        function closeAlert() {
+            document.getElementById("overlay").style.display = "none";
+            document.getElementById("content").style.visibility = "visible";  
+            
+            document.body.style.overflowY = "auto";
+            AOS.refreshHard(); 
+        }
+        
